315b7ea00d5faf142100dbe956119d7376c50ac5
karan-rawal <kr14nov@outlook.com>
2017-06-11 16:22:29 +0530

    Events handling.

--- react/reactJS/reactjs-basics/src/app/components/Home.js
+++ react/reactJS/reactjs-basics/src/app/components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 export class Home extends React.Component{
+
+  constructor(props){
+    super();
+    this.age = props.age;
+  }
+
+  onMakeOlder(){
+    this.age += 3;
+    console.log(this.age);
+  }
+
   render(){
 
     return(
       <div>
         <p>In a new component.</p>
-        <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+        <p>Your name is {this.props.name}, your age is { this.age }</p>
+        <hr/>
+        <button onClick={ this.onMakeOlder.bind(this) } className="btn btn-primary">Make me older.</button>
       </div>
     )
   }
