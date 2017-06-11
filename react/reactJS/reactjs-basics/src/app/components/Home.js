0a3c7c4028fe5c3279d722bad5ee75a3b43cf3b2
karan-rawal <kr14nov@outlook.com>
2017-06-11 16:46:54 +0530

    Changing the state of a component. (Updating the UI).

--- react/reactJS/reactjs-basics/src/app/components/Home.js
+++ react/reactJS/reactjs-basics/src/app/components/Home.js
@@ -4,12 +4,23 @@ export class Home extends React.Component{
 
   constructor(props){
     super();
-    this.age = props.age;
+
+    this.state = {
+      age: props.initialAge,
+      status : 0
+    };
+
+    setTimeout(() => {
+      this.setState({
+        status : 1
+      });
+    }, 3000)
   }
 
   onMakeOlder(){
-    this.age += 3;
-    console.log(this.age);
+    this.setState({
+      age: this.state.age + 3
+    });
   }
 
   render(){
@@ -17,7 +28,8 @@ export class Home extends React.Component{
     return(
       <div>
         <p>In a new component.</p>
-        <p>Your name is {this.props.name}, your age is { this.age }</p>
+        <p>Your name is {this.props.name}, your age is { this.state.age }</p>
+        <p>Status : { this.state.status }</p>
         <hr/>
         <button onClick={ this.onMakeOlder.bind(this) } className="btn btn-primary">Make me older.</button>
       </div>
--- react/reactJS/reactjs-basics/src/app/index.js
+++ react/reactJS/reactjs-basics/src/app/index.js
@@ -15,7 +15,7 @@ class App extends React.Component{
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name={ "Max" } age={ 18 }>
+            <Home name={ "Max" } initialAge={ 18 }>
             </Home>
           </div>
         </div>
