28b5b9a533267c82e0319c4a7c3a0a644c2836f8
karan-rawal <kr14nov@outlook.com>
2017-06-11 15:23:09 +0530

    Added bootstrap styles.

--- react/reactJS/reactjs-basics/src/app/index.js
+++ react/reactJS/reactjs-basics/src/app/index.js
@@ -4,8 +4,12 @@ import { render } from "react-dom";
 class App extends React.Component{
   render() {
     return (
-      <div>
-        <h1>Hello!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <h1>Hello!</h1>
+          </div>
+        </div>
       </div>
     );
   }
