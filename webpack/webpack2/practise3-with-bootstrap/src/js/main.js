0bab28f74de993c6d0e2aa55227d11ada709c262
Karan Rawal <kr14nov@outlook.com>
2017-06-21 14:51:18 +0530

    Added react-bootstrap.

--- webpack/webpack2/practise3-with-bootstrap/src/js/main.js
+++ webpack/webpack2/practise3-with-bootstrap/src/js/main.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Col, Alert } from 'react-bootstrap';
 
 require('../stylesheets/main.scss');
 require('../stylesheets/main.scss');
 require('bootstrap-sass/assets/stylesheets/_bootstrap.scss');
 
-class AppComponent extends React.Component{
-    render(){
-        return(
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-8">
-                        <p>Col sm 8</p>
-                    </div>
-                    <div className="col-sm-4">
-                        Col sm 4
-                    </div>
-                </div>
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Col md={8}>
+                    <p>Hello Karan</p>
+                </Col>
+                <Col md={4}>
+                    <p>Hello Karan 2</p>
+                    <Alert bsStyle="danger">KAran I love you</Alert>
+                </Col>
             </div>
         );
     }
 }
 
-ReactDOM.render(<AppComponent/>, document.getElementById("root"));
+ReactDOM.render(<AppComponent />, document.getElementById("root"));
