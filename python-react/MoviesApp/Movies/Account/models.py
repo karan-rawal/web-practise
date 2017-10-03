b4b2353579344992c1fbc0642079ca72f108c86a
Karan Rawal <karan.rawal@springct.com>
2017-10-03 15:57:11 +0530

    Created Account model.

--- python-react/MoviesApp/Movies/Account/models.py
+++ python-react/MoviesApp/Movies/Account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
+class Account(AbstractUser):
+    first_name = models.CharField(max_length = 100, null = False, blank = False)
+    password = models.CharField(max_length = 20, null = False, blank = False)
