2cebf440d381817e19c49d8cd7bc73987663c809
Karan Rawal <karan.rawal@springct.com>
2017-10-05 13:08:53 +0530

    Added jwt authentication.

--- python-react/MoviesApp/Movies/Account/urls.py
+++ python-react/MoviesApp/Movies/Account/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from .views import AccountView
+from .views import AccountView, AccountRegistrationView
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    url(r'^$', AccountView.as_view())
+    url(r'^$', AccountView.as_view()),
+    url(r'^auth/login', obtain_jwt_token),
+    url(r'^auth/token-refresh/', refresh_jwt_token),
+    url(r'^auth/token-verify/', verify_jwt_token),
+    url(r'^auth/register', AccountRegistrationView.as_view())
 ]
--- python-react/MoviesApp/Movies/Account/views.py
+++ python-react/MoviesApp/Movies/Account/views.py
@@ -4,6 +4,7 @@ from .models import Account
 from .serializers import AccountSerializer, AccountRegistrationSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class AccountView(APIView):
@@ -12,6 +13,8 @@ class AccountView(APIView):
         serializer = AccountSerializer(accounts, many = True)
         return Response(serializer.data, status.HTTP_200_OK)
 
+class AccountRegistrationView(APIView):
+    permission_classes = (AllowAny,)
     def post(self, request):
         serializer = AccountRegistrationSerializer(data = request.data)
         if(serializer.is_valid()):
