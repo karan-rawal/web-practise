5c26c4866f0805a56e32bd5ff54ed35b270b5400
Karan Rawal <karan.rawal@springct.com>
2017-10-11 16:45:12 +0530

    Changing to login screen after some interval.

--- react/react-native/Basics/basics1/src/js/splash/containers/Splash.Container.js
+++ react/react-native/Basics/basics1/src/js/splash/containers/Splash.Container.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Actions } from 'react-native-router-flux';
 import Splash from '../components/Splash.Component';
+import Constants from '../Constants';
 
 export default class SplashContainer extends React.Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            Actions.pop();
+            Actions.KEY_LOGIN();
+        }, Constants.SPLASH_DURATION);
+    }
+
     render() {
         return (
             <Splash />
